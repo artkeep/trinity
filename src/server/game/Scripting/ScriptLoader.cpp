b59d9f1ed79dd44d53ee95021ebbf11135556148
artkeep <artkeep@yandex.ru>
2011-02-02 17:20:58 +0200

    Outdoor Grizzly Hills add

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -605,6 +605,7 @@ void AddSC_outdoorpvp_si();
 void AddSC_outdoorpvp_tf();
 void AddSC_outdoorpvp_zm();
 void AddSC_outdoorpvp_wg();
+void AddSC_outdoorpvp_gh();
 
 // player
 void AddSC_chat_log();
@@ -1233,6 +1234,7 @@ void AddOutdoorPvPScripts()
     AddSC_outdoorpvp_tf();
     AddSC_outdoorpvp_zm();
 	AddSC_outdoorpvp_wg();
+    AddSC_outdoorpvp_gh();
 #endif
 }
 

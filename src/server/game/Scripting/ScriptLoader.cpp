57eb4cdb23583973274d9a4881a70dd6d9829ecb
artkeep <artkeep@yandex.ru>
2011-02-03 18:09:21 +0200

    Скрипт лотерейщика.

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -79,6 +79,7 @@ void AddSC_npc_innkeeper();
 void AddSC_npcs_special();
 void AddSC_npc_taxi();
 void AddSC_achievement_scripts();
+void AddSC_boss_lottery();
 
 //eastern kingdoms
 void AddSC_alterac_valley();                 //Alterac Valley
@@ -694,6 +695,7 @@ void AddWorldScripts()
     AddSC_npc_taxi();
     AddSC_achievement_scripts();
     AddSC_chat_log();
+    AddSC_boss_lottery();
 #endif
 }
 

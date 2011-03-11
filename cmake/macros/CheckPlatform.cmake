75970b7b25ae711f6f5f173513954002ba0fb1d7
tobmaps <spambot42@yandex.ru>
2011-03-11 21:05:38 +0600

    removed [p1]

--- cmake/macros/CheckPlatform.cmake
+++ cmake/macros/CheckPlatform.cmake
@@ -1,12 +1,11 @@
 # check what platform we're on (64-bit or 32-bit), and create a simpler test than CMAKE_SIZEOF_VOID_P
-# for my bugged system i should avoid this stupid check to be able compile in 64 mode :(
-#if(CMAKE_SIZEOF_VOID_P MATCHES 8)
+if(CMAKE_SIZEOF_VOID_P MATCHES 8)
     set(PLATFORM 64)
     MESSAGE(STATUS "Detected 64-bit platform")
-#else()
-#    set(PLATFORM 32)
-#    MESSAGE(STATUS "Detected 32-bit platform")
-#endif()
+else()
+    set(PLATFORM 32)
+    MESSAGE(STATUS "Detected 32-bit platform")
+endif()
 
 include("${CMAKE_SOURCE_DIR}/cmake/platform/settings.cmake")
 

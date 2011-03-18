83f4d3dd7e3ce1a277010bf3988221f52d012659
tobmaps <spambot42@yandex.ru>
2011-03-19 03:25:35 +0600

    [p9] Fixed initial damage of druid's Rake ability. Patch by me

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -4130,6 +4130,9 @@ void SpellMgr::LoadSpellCustomAttr()
                 // Roar
                 else if (spellInfo->SpellFamilyFlags[0] & 0x8)
                     mSpellCustomAttr[i] |= SPELL_ATTR0_CU_AURA_CC;
+                // Rake
+                else if (spellInfo->SpellFamilyFlags[0] & 0x1000 && spellInfo->SpellIconID == 494)
+                    mSpellCustomAttr[i] |= SPELL_ATTR0_CU_IGNORE_ARMOR;
                 else
                     break;
                 count++;

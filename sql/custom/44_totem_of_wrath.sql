0ec15a17551ebdb768a99cee3049982ef258998d
tobmaps <spambot42@yandex.ru>
2011-03-30 02:43:33 +0700

    [p44] replaced by more correct version

--- sql/custom/44_totem_of_wrath.sql
+++ sql/custom/44_totem_of_wrath.sql
@@ -1,6 +1,1 @@
-DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (57658,57660,57662,57663);
-INSERT INTO `spell_linked_spell` VALUES
-(57658,30708,0,'Totem of Wrath'),
-(57660,30708,0,'Totem of Wrath'),
-(57662,30708,0,'Totem of Wrath'),
-(57663,30708,0,'Totem of Wrath');
+UPDATE `creature_template` SET `spell2` = 30708 WHERE `spell1` IN (57658,57660,57662,57663);

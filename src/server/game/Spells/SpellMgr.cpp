22871b40e35050ce5c30d00f717f2f22b83503d2
tobmaps <spambot42@yandex.ru>
2011-04-01 16:28:11 +0700

    [p52] Fixed priest's talent Twin Disciplines. Now it will affect also at Prayer of Mending. Patch by me

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -4186,6 +4186,14 @@ void SpellMgr::LoadSpellCustomAttr()
                     spellInfo->SpellFamilyFlags[0] |= 0x40;
                 count++;
                 break;
+            case SPELLFAMILY_PRIEST:
+                // Twin Disciplines should affect at Prayer of Mending
+                if (spellInfo->SpellIconID == 2292)
+                    spellInfo->EffectSpellClassMask[0][1] |= 0x20;
+                else
+                    break;
+                count++;
+                break;
         }
     }
 

c7089027be9e1271706afb1bd7fcb4d1bedf72b3
tobmaps <spambot42@yandex.ru>
2011-04-15 22:08:18 +0700

    Revert [p31]. Will be replaced by more correct version

--- sql/custom/31_misdirection.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM `spell_proc_event` WHERE `entry` = 34477;
-INSERT INTO `spell_proc_event` (`entry`,`cooldown`) VALUES (34477,30);
-
--- remove this when you find a way how to make 35079 to redirect aggro
-DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = -35079;
-INSERT INTO `spell_linked_spell` VALUES (-35079,-34477,0,'Misdirection');

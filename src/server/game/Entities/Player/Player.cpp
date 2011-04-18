3c34dd29fc57f249e7f166a0a5f3134feb6b45d9
tobmaps <spambot42@yandex.ru>
2011-04-18 20:27:51 +0700

    [p105] Reimplemented some chat channel flags. Now you can't join into "Looking For Group" channel from anywhere. Patch by me

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -5498,14 +5498,8 @@ void Player::RepopAtGraveyard()
 
 bool Player::CanJoinConstantChannelInZone(ChatChannelsEntry const* channel, AreaTableEntry const* zone)
 {
-    if (channel->flags & CHANNEL_DBC_FLAG_ZONE_DEP)
-    {
-        if (zone->flags & AREA_FLAG_ARENA_INSTANCE)
-            return false;
-
-        if ((channel->flags & CHANNEL_DBC_FLAG_CITY_ONLY) && !(zone->flags & AREA_FLAG_CAPITAL))
-            return false;
-    }
+    if (channel->flags & (CHANNEL_DBC_FLAG_CITY_ONLY) && !(zone->flags & AREA_FLAG_CAPITAL))
+        return false;
 
     return true;
 }
@@ -5553,8 +5547,8 @@ void Player::UpdateLocalChannels(uint32 newZone)
     {
         if (ChatChannelsEntry const* channel = sChatChannelsStore.LookupEntry(i))
         {
-            if (!(channel->flags & CHANNEL_DBC_FLAG_ZONE_DEP))
-                continue;                                    // Not zone dependent, don't handle it here
+            if (!(channel->flags & CHANNEL_DBC_FLAG_ZONE_UPD))
+                continue;                                    // No zone update, don't handle it here
 
             if ((channel->flags & CHANNEL_DBC_FLAG_GUILD_REQ) && GetGuildId())
                 continue;                                    // Should not join to these channels automatically
@@ -5584,7 +5578,7 @@ void Player::UpdateLocalChannels(uint32 newZone)
                     char new_channel_name_buf[100];
                     char const* currentNameExt;
 
-                    if (channel->flags & CHANNEL_DBC_FLAG_CITY_ONLY)
+                    if (channel->flags & CHANNEL_DBC_FLAG_CITY_NAME)
                         currentNameExt = sObjectMgr->GetTrinityStringForDBCLocale(LANG_CHANNEL_CITY);
                     else
                         currentNameExt = current_zone_name.c_str();

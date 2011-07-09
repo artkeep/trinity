a62d37d5ccb4e7693e419d48ed84b7ccbd4d72fd
tobmaps <spambot42@yandex.ru>
2011-07-09 14:37:52 +0700

    Revert [p140]. Will be replaced by new version

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -330,7 +330,6 @@ class WorldSession
             }
         }
         //used with item_page table
-        static void SendExternalMails();
         bool SendItemInfo(uint32 itemid, WorldPacket data);
         //auction
         void SendAuctionHello(uint64 guid, Creature* unit);

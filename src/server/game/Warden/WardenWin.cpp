0edcc5360958c34f7e927e5ce6cdf822eaf00f96
Chaplain <Erfo@.(none)>
2011-07-14 23:48:52 +0300

    added Warden.BanDay

--- src/server/game/Warden/WardenWin.cpp
+++ src/server/game/Warden/WardenWin.cpp
@@ -489,6 +489,15 @@ void WardenWin::HandleData(ByteBuffer &buff)
         }
     }
 
-    if (found && sWorld->getBoolConfig(CONFIG_BOOL_WARDEN_KICK))
+    uint32 banDay = sWorld->getIntConfig(CONFIG_INT_WARDEN_BANDAY);
+
+    if (found && banDay)
+    {
+        std::string sDuratuin = (std::string(banDay + "d"));
+        std::string sText = ("Игрок: " + std::string(Client->GetPlayerName()) + " использовал читерское ПО и был забанен на " + sDuratuin.c_str());
+        sWorld->SendGMText(LANG_GM_BROADCAST, sText.c_str());
+        sWorld->BanAccount(BAN_CHARACTER, Client->GetPlayerName(), sDuratuin.c_str(), "Cheating software user", "Server guard");
+    }
+    else if (found && sWorld->getBoolConfig(CONFIG_BOOL_WARDEN_KICK))
         Client->KickPlayer();
 }

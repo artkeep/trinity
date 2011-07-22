2ba3c93303fbf4bd329526faa554b9cac796b02a
tobmaps <spambot42@yandex.ru>
2011-07-23 00:30:13 +0700

    Added lost part of [p217]

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -42,7 +42,7 @@
 #include "CreatureAISelector.h"
 #include "Group.h"
 
-GameObject::GameObject() : WorldObject(), m_goValue(new GameObjectValue), m_AI(NULL)
+GameObject::GameObject() : WorldObject(), m_goValue(new GameObjectValue), m_AI(NULL), IsTemporary(false)
 {
     m_objectType |= TYPEMASK_GAMEOBJECT;
     m_objectTypeId = TYPEID_GAMEOBJECT;

5a4ac3d0bf56b93a0f67efca00a15f4979c0c002
Chaplain <Chaplain@.(none)>
2011-07-25 21:39:46 +0300

    Revert "Core/Spells: Tricks of the trade spell fix. Thanks DrTenma"
    This reverts commit ab9637dcbeb5331b3d23b602b84de5617f363e28.
    
    Conflicts:
    
    	src/server/game/Spells/Auras/SpellAuraEffects.cpp

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -6655,17 +6655,6 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
                     triggered_spell_id = 32747;
                     break;
                 }
-                case 57934: // Tricks of the Trade
-                {
-                    if (Unit * unitTarget = GetMisdirectionTarget())
-                    {
-                        RemoveAura(dummySpell->Id, GetGUID(), 0, AURA_REMOVE_BY_DEFAULT);
-                        CastSpell(this, 59628, true);
-                        CastSpell(unitTarget, 57933, true);
-                        return true;
-                    }
-                    return false;
-                }
             }
             // Cut to the Chase
             if (dummySpell->SpellIconID == 2909)

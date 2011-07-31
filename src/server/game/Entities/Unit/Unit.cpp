499bebf7226e76b4ccc2cc99b269dbd0bbf9a836
Chaplain <Chaplain@.(none)>
2011-08-01 00:49:52 +0300

    Fix logic in SPELL_AURA_MOD_DAMAGE_PERCENT_DONE patch by me
    for example it fixes Two-Handed Weapon Specialization

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10663,7 +10663,7 @@ uint32 Unit::SpellDamageBonus(Unit* victim, SpellEntry const* spellProto, uint32
             {
                 if ((*i)->GetSpellProto()->EquippedItemClass & spellProto->EquippedItemClass)
                     if (((*i)->GetSpellProto()->EquippedItemSubClassMask == 0) ||
-                        ((*i)->GetSpellProto()->EquippedItemSubClassMask & spellProto->EquippedItemSubClassMask))
+                        (ToPlayer() && ToPlayer()->HasItemFitToSpellRequirements((*i)->GetSpellProto())))
                         AddPctN(DoneTotalMod, (*i)->GetAmount());
             }
             else if (ToPlayer() && ToPlayer()->HasItemFitToSpellRequirements((*i)->GetSpellProto()))
@@ -12029,12 +12029,12 @@ void Unit::MeleeDamageBonus(Unit* victim, uint32 *pdamage, WeaponAttackType attT
                 {
                     if ((*i)->GetSpellProto()->EquippedItemClass & spellProto->EquippedItemClass)
                         if (((*i)->GetSpellProto()->EquippedItemSubClassMask == 0) ||
-                            ((*i)->GetSpellProto()->EquippedItemSubClassMask & spellProto->EquippedItemSubClassMask))
+                            (ToPlayer() && ToPlayer()->HasItemFitToSpellRequirements((*i)->GetSpellProto())))
                             AddPctN(DoneTotalMod, (*i)->GetAmount());
                 }
                 else if (ToPlayer() && ToPlayer()->HasItemFitToSpellRequirements((*i)->GetSpellProto()))
                     AddPctN(DoneTotalMod, (*i)->GetAmount());
-                }
+            }
         }
         else if (player)
         {

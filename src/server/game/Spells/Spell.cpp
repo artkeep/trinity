e8463ba9681bd5c5ba1f9ca167e0363e3a2db3d3
Chaplain <aionthefirst@gmail.com>
2011-09-23 20:51:34 +0300

    Revert "[p166] Prevent totems to be put out of LoS. Patch by simlt"
    This reverts commit 2a2a12bd9dbe6f2aca55a5de6adfba641a00dead.
    
    Conflicts:
    
    	src/server/game/Spells/Spell.cpp

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -2272,20 +2272,10 @@ void Spell::SelectEffectTargets(uint32 i, SpellImplicitTargetInfo const& cur)
             }
 
             Position pos;
-            switch (cur.GetTarget())
-            {
-                case TARGET_DEST_CASTER_FRONT_LEAP:
-                case TARGET_DEST_CASTER_FRONT_LEFT:
-                case TARGET_DEST_CASTER_BACK_LEFT:
-                case TARGET_DEST_CASTER_BACK_RIGHT:
-                case TARGET_DEST_CASTER_FRONT_RIGHT:
-                    m_caster->GetFirstCollisionPosition(pos, dist, angle);
-                    break;
-                default:
-                    m_caster->GetNearPosition(pos, dist, angle);
-                    break;
-            }
-
+            if (cur.GetTarget() == TARGET_DEST_CASTER_FRONT_LEAP)
+                m_caster->GetFirstCollisionPosition(pos, dist, angle);
+            else
+                m_caster->GetNearPosition(pos, dist, angle);
             m_targets.SetDst(*m_caster);
             m_targets.ModDst(pos);
             break;

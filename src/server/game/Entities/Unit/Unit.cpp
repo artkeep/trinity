cd87e26f4bf169dbe23eef1aed343c644fc7d6e2
Chaplain <aionthefirst@gmail.com>
2011-12-10 16:18:28 +0300

    Revert "Spells/Warrior: Finally fix Warrior T10 Melee 4P Bonus"
    This reverts commit 085c8d8d17bfd92595db8a47ec3d93e622be9ef8.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -9083,7 +9083,7 @@ bool Unit::HandleProcTriggerSpell(Unit* victim, uint32 damage, AuraEffect* trigg
             // Item - Warrior T10 Melee 4P Bonus
             if (AuraEffect const * aurEff = GetAuraEffect(70847, EFFECT_0))
             {
-                int32 amount = aurEff->GetAmount();
+                uint32 amount = aurEff->GetAmount();
                 if (roll_chance_i(amount))
                     CastSpell(this, 70849, true, castItem, triggeredByAura); // Extra Charge!
                 if (roll_chance_i(amount))

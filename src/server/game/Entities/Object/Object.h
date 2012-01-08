de3652b825c4805795d6a3c84afabd64dccf576c
Havenard <havenard@hotmail.com>
2012-01-09 00:09:12 +0300

    Remove assertion check until this crash is figured out

--- src/server/game/Entities/Object/Object.h
+++ src/server/game/Entities/Object/Object.h
@@ -529,7 +529,7 @@ class GridObject
     public:
         bool IsInGrid() const { return _gridRef.isValid(); }
         void AddToGrid(GridRefManager<T>& m) { ASSERT(!IsInGrid()); _gridRef.link(&m, (T*)this); }
-        void RemoveFromGrid() { ASSERT(IsInGrid()); _gridRef.unlink(); }
+        void RemoveFromGrid() { _gridRef.unlink(); }
     private:
         GridReference<T> _gridRef;
 };

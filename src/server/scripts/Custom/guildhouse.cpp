05a6462ce83fa14822470e335faec71aed407647
Chaplain <aionthefirst@gmail.com>
2012-01-09 20:02:34 +0300

    Some cleanup

--- src/server/scripts/Custom/guildhouse.cpp
+++ src/server/scripts/Custom/guildhouse.cpp
@@ -54,312 +54,312 @@ char str[200];
 
 bool isPlayerGuildLeader(Player *player)
 {
- return (player->GetRank() == 0) && (player->GetGuildId() != 0);
+    return (player->GetRank() == 0) && (player->GetGuildId() != 0);
 }
 
 bool getGuildHouseCoords(uint32 guildId, float &x, float &y, float &z, uint32 &map)
 {
- if (guildId == 0)
- {
- //if player has no guild
- return false;
- }
+    if (guildId == 0)
+    {
+        //if player has no guild
+        return false;
+    }
 
- QueryResult result;
- result = CharacterDatabase.PQuery("SELECT `x`, `y`, `z`, `map` FROM `guildhouses` WHERE `guildId` = %u", guildId);
- if (result)
- {
- Field *fields = result->Fetch();
- x = fields[0].GetFloat();
- y = fields[1].GetFloat();
- z = fields[2].GetFloat();
- map = fields[3].GetUInt32();
- return true;
- }
+    QueryResult result;
+    result = CharacterDatabase.PQuery("SELECT `x`, `y`, `z`, `map` FROM `guildhouses` WHERE `guildId` = %u", guildId);
+    if (result)
+    {
+        Field *fields = result->Fetch();
+        x = fields[0].GetFloat();
+        y = fields[1].GetFloat();
+        z = fields[2].GetFloat();
+        map = fields[3].GetUInt32();
+        return true;
+    }
 
- return false;
+    return false;
 }
 
 void teleportPlayerToGuildHouse(Player *player, Creature *_creature)
 {
- if (player->GetGuildId() == 0)
- {
- //if player has no guild
- _creature->MonsterWhisper(MSG_NOTINGUILD, player->GetGUID());
- sprintf(str,"Мне жаль %u вы должны состоять в гильдии!", player->GetName());
- return;
- }
+    if (player->GetGuildId() == 0)
+    {
+        //if player has no guild
+        _creature->MonsterWhisper(MSG_NOTINGUILD, player->GetGUID());
+        sprintf(str,"Мне жаль %u вы должны состоять в гильдии!", player->GetName());
+        return;
+    }
 
- if (!player->getAttackers().empty())
- {
- //if player in combat
- _creature->MonsterSay(MSG_INCOMBAT, LANG_UNIVERSAL, player->GetGUID());
- return;
- }
+    if (!player->getAttackers().empty())
+    {
+        //if player in combat
+        _creature->MonsterSay(MSG_INCOMBAT, LANG_UNIVERSAL, player->GetGUID());
+        return;
+    }
 
- float x, y, z;
- uint32 map;
+    float x, y, z;
+    uint32 map;
 
- if (getGuildHouseCoords(player->GetGuildId(), x, y, z, map))
- {
- //teleport player to the specified location
- player->TeleportTo(map, x, y, z, 0.0f);
- }
- else
- _creature->MonsterWhisper(MSG_NOGUILDHOUSE, player->GetGUID());
+    if (getGuildHouseCoords(player->GetGuildId(), x, y, z, map))
+    {
+        //teleport player to the specified location
+        player->TeleportTo(map, x, y, z, 0.0f);
+    }
+    else
+        _creature->MonsterWhisper(MSG_NOGUILDHOUSE, player->GetGUID());
 }
 
 bool showBuyList(Player *player, Creature *_creature, uint32 showFromId = 0)
 {
- //show not occupied GuildHouses
- 
- if(player->HasItemCount(20880, 1))
- {
+    //show not occupied GuildHouses
 
- QueryResult result;
- result = CharacterDatabase.PQuery("SELECT `id`, `comment` FROM `guildhouses` WHERE `guildId` = 0 AND `id` > %u ORDER BY `id` ASC LIMIT %u",
- showFromId, GOSSIP_COUNT_MAX);
+    if(player->HasItemCount(20880, 1))
+    {
 
- if (result)
- {
- uint32 guildhouseId = 0;
- std::string comment = "";
- do
- {
- Field *fields = result->Fetch();
- guildhouseId = fields[0].GetInt32();
- comment = fields[1].GetString();
+        QueryResult result;
+        result = CharacterDatabase.PQuery("SELECT `id`, `comment` FROM `guildhouses` WHERE `guildId` = 0 AND `id` > %u ORDER BY `id` ASC LIMIT %u",
+            showFromId, GOSSIP_COUNT_MAX);
 
- //send comment as a gossip item
- //transmit guildhouseId in Action variable
- player->ADD_GOSSIP_ITEM(ICON_GOSSIP_TABARD, comment, GOSSIP_SENDER_MAIN,
- guildhouseId + OFFSET_GH_ID_TO_ACTION);
- }
- while (result->NextRow());
+        if (result)
+        {
+            uint32 guildhouseId = 0;
+            std::string comment = "";
+            do
+            {
+                Field *fields = result->Fetch();
+                guildhouseId = fields[0].GetInt32();
+                comment = fields[1].GetString();
 
- if (result->GetRowCount() == GOSSIP_COUNT_MAX)
- {
- //assume that we have additional page
- //add link to next GOSSIP_COUNT_MAX items
- player->ADD_GOSSIP_ITEM(ICON_GOSSIP_BALOONDOTS, MSG_GOSSIP_NEXTPAGE, GOSSIP_SENDER_MAIN,
- guildhouseId + OFFSET_SHOWBUY_FROM);
- }
+                //send comment as a gossip item
+                //transmit guildhouseId in Action variable
+                player->ADD_GOSSIP_ITEM(ICON_GOSSIP_TABARD, comment, GOSSIP_SENDER_MAIN,
+                    guildhouseId + OFFSET_GH_ID_TO_ACTION);
+            }
+            while (result->NextRow());
 
- player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, _creature->GetGUID());
+            if (result->GetRowCount() == GOSSIP_COUNT_MAX)
+            {
+                //assume that we have additional page
+                //add link to next GOSSIP_COUNT_MAX items
+                player->ADD_GOSSIP_ITEM(ICON_GOSSIP_BALOONDOTS, MSG_GOSSIP_NEXTPAGE, GOSSIP_SENDER_MAIN,
+                    guildhouseId + OFFSET_SHOWBUY_FROM);
+            }
 
- return true;
- }
- else
- {
- if (showFromId = 0)
- {
- //all GuildHouses are occupied
- _creature->MonsterWhisper(MSG_NOFREEGH, player->GetGUID());
- player->CLOSE_GOSSIP_MENU();
- }
- else
- {
- //this condition occurs when COUNT(guildhouses) % GOSSIP_COUNT_MAX == 0
- //just show GHs from beginning
- showBuyList(player, _creature, 0);
- }
- }
- } else {
- _creature->MonsterSay(MSG_NO_TOKEN, LANG_UNIVERSAL, player->GetGUID());
- sprintf(str,"У вас должен быть Golden token для просмотра списка.");
- player->CLOSE_GOSSIP_MENU();
- }
+            player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, _creature->GetGUID());
 
- return false;
+            return true;
+        }
+        else
+        {
+            if (showFromId = 0)
+            {
+                //all GuildHouses are occupied
+                _creature->MonsterWhisper(MSG_NOFREEGH, player->GetGUID());
+                player->CLOSE_GOSSIP_MENU();
+            }
+            else
+            {
+                //this condition occurs when COUNT(guildhouses) % GOSSIP_COUNT_MAX == 0
+                //just show GHs from beginning
+                showBuyList(player, _creature, 0);
+            }
+        }
+    } else {
+        _creature->MonsterSay(MSG_NO_TOKEN, LANG_UNIVERSAL, player->GetGUID());
+        sprintf(str,"У вас должен быть Golden token для просмотра списка.");
+        player->CLOSE_GOSSIP_MENU();
+    }
+
+    return false;
 }
 
 bool isPlayerHasGuildhouse(Player *player, Creature *_creature, bool whisper = false)
 {
- QueryResult result;
- result = CharacterDatabase.PQuery("SELECT `comment` FROM `guildhouses` WHERE `guildId` = %u",
- player->GetGuildId());
+    QueryResult result;
+    result = CharacterDatabase.PQuery("SELECT `comment` FROM `guildhouses` WHERE `guildId` = %u",
+        player->GetGuildId());
 
- if (result)
- {
- if (whisper)
- {
- //whisper to player "already have etc..."
- Field *fields = result->Fetch();
- char msg[100];
- sprintf(msg, MSG_ALREADYHAVEGH, fields[0].GetString());
- _creature->MonsterWhisper(msg, player->GetGUID());
- }
+    if (result)
+    {
+        if (whisper)
+        {
+            //whisper to player "already have etc..."
+            Field *fields = result->Fetch();
+            char msg[100];
+            sprintf(msg, MSG_ALREADYHAVEGH, fields[0].GetString());
+            _creature->MonsterWhisper(msg, player->GetGUID());
+        }
 
- return true;
- }
+        return true;
+    }
 
- return false;
+    return false;
 }
 
 void buyGuildhouse(Player *player, Creature *_creature, uint32 guildhouseId)
 {
- if (player->GetMoney() < COST_GH_BUY)
- {
- //show how much money player need to buy GH (in gold)
- char msg[100];
- sprintf(msg, MSG_NOTENOUGHMONEY, COST_GH_BUY / 10000);
- _creature->MonsterWhisper(msg, player->GetGUID());
- return;
- }
+    if (player->GetMoney() < COST_GH_BUY)
+    {
+        //show how much money player need to buy GH (in gold)
+        char msg[100];
+        sprintf(msg, MSG_NOTENOUGHMONEY, COST_GH_BUY / 10000);
+        _creature->MonsterWhisper(msg, player->GetGUID());
+        return;
+    }
 
- if (isPlayerHasGuildhouse(player, _creature, true))
- {
- //player already have GH
- return;
- }
+    if (isPlayerHasGuildhouse(player, _creature, true))
+    {
+        //player already have GH
+        return;
+    }
 
- QueryResult result;
+    QueryResult result;
 
- //check if somebody already occupied this GH
- result = CharacterDatabase.PQuery("SELECT `id` FROM `guildhouses` WHERE `id` = %u AND `guildId` <> 0", guildhouseId);
+    //check if somebody already occupied this GH
+    result = CharacterDatabase.PQuery("SELECT `id` FROM `guildhouses` WHERE `id` = %u AND `guildId` <> 0", guildhouseId);
 
- if (result)
- {
- _creature->MonsterWhisper(MSG_GHOCCUPIED, player->GetGUID());
- return;
- }
+    if (result)
+    {
+        _creature->MonsterWhisper(MSG_GHOCCUPIED, player->GetGUID());
+        return;
+    }
 
- //update DB
- result = CharacterDatabase.PQuery("UPDATE `guildhouses` SET `guildId` = %u WHERE `id` = %u",
- player->GetGuildId(), guildhouseId);
+    //update DB
+    result = CharacterDatabase.PQuery("UPDATE `guildhouses` SET `guildId` = %u WHERE `id` = %u",
+        player->GetGuildId(), guildhouseId);
 
- if (result)
- player->ModifyMoney(-COST_GH_BUY);
- player->DestroyItemCount(20880, 1, true, false);
- _creature->MonsterSay(MSG_CONGRATULATIONS, LANG_UNIVERSAL, player->GetGUID());
+    if (result)
+        player->ModifyMoney(-COST_GH_BUY);
+    player->DestroyItemCount(20880, 1, true, false);
+    _creature->MonsterSay(MSG_CONGRATULATIONS, LANG_UNIVERSAL, player->GetGUID());
 }
 
 void sellGuildhouse(Player *player, Creature *_creature)
 {
- QueryResult result;
- result = CharacterDatabase.PQuery("UPDATE `guildhouses` SET `guildId` = 0 WHERE `guildId` = %u", player->GetGuildId());
+    QueryResult result;
+    result = CharacterDatabase.PQuery("UPDATE `guildhouses` SET `guildId` = 0 WHERE `guildId` = %u", player->GetGuildId());
 
- if (result)
- player->ModifyMoney(COST_GH_SELL);
- _creature->MonsterSay(MSG_SOLD, LANG_UNIVERSAL, player->GetGUID());
- player->CLOSE_GOSSIP_MENU();
+    if (result)
+        player->ModifyMoney(COST_GH_SELL);
+    _creature->MonsterSay(MSG_SOLD, LANG_UNIVERSAL, player->GetGUID());
+    player->CLOSE_GOSSIP_MENU();
 }
 
 class guildmaster : public CreatureScript
 {
- public:
- guildmaster() : CreatureScript("guildmaster") { }
+public:
+    guildmaster() : CreatureScript("guildmaster") { }
 
- bool GossipSelectWithCode(Player *player, Creature *_creature, uint32 sender, uint32 action, const char* sCode)
- {
- if (sender == GOSSIP_SENDER_MAIN)
- {
- if (action == ACTION_SELL_GUILDHOUSE)
- {
- int i = -1;
- try
- {
- //compare code
-
- if (strlen(sCode) + 1 == sizeof CODE_SELL)
- i = strcmp(CODE_SELL, sCode);
- }
-
- catch(char *str) {sLog->outErrorDb(str);
- }
-
- if (i == 0)
- {
- //right code
- sellGuildhouse(player, _creature);
- }
-
- player->CLOSE_GOSSIP_MENU();
- return true;
- }
- }
- return false;
-}
-
- bool OnGossipSelect(Player *player, Creature *_creature, uint32 sender, uint32 action)
- {
- player->PlayerTalkClass->ClearMenus();
- if (sender != GOSSIP_SENDER_MAIN)
- return false;
-
- switch (action)
- {
- case ACTION_TELE:
- //teleport player to GH
- player->CLOSE_GOSSIP_MENU();
- teleportPlayerToGuildHouse(player, _creature);
- break;
- case ACTION_SHOW_BUYLIST:
- //show list of GHs which currently not occupied
- showBuyList(player, _creature);
- break;
-
- case ACTION_SELL_GUILDHOUSE_OPT:
- player->CLOSE_GOSSIP_MENU();
- player->ADD_GOSSIP_ITEM(0, "Да, Продать Гильдхаус", GOSSIP_SENDER_MAIN, ACTION_SELL_GUILDHOUSE_YES);
- player->ADD_GOSSIP_ITEM(0, "Нет, закрыть окно", GOSSIP_SENDER_MAIN, ACTION_SELL_GUILDHOUSE_NO);
- player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, _creature->GetGUID());
- break;
- 
- case ACTION_SELL_GUILDHOUSE_YES:
- sellGuildhouse(player, _creature);
- player->CLOSE_GOSSIP_MENU();
- break;
- 
- case ACTION_SELL_GUILDHOUSE_NO:
- player->CLOSE_GOSSIP_MENU();
- break;
-
- default:
- if (action > OFFSET_SHOWBUY_FROM)
- {
- showBuyList(player, _creature, action - OFFSET_SHOWBUY_FROM);
- }
- else if (action > OFFSET_GH_ID_TO_ACTION)
- {
- //player clicked on buy list
- player->CLOSE_GOSSIP_MENU();
-
- //get guildhouseId from action
- //guildhouseId = action - OFFSET_GH_ID_TO_ACTION
- buyGuildhouse(player, _creature, action - OFFSET_GH_ID_TO_ACTION);
- }
- break;
- }
-
- return true;
- }
-
-
- bool OnGossipHello(Player *player, Creature *_creature)
- {
-    player->ADD_GOSSIP_ITEM(ICON_GOSSIP_BALOON, MSG_GOSSIP_TELE, GOSSIP_SENDER_MAIN, ACTION_TELE);
-
-    if (isPlayerGuildLeader(player))
+    bool GossipSelectWithCode(Player *player, Creature *_creature, uint32 sender, uint32 action, const char* sCode)
     {
-        if (isPlayerHasGuildhouse(player, _creature))
+        if (sender == GOSSIP_SENDER_MAIN)
         {
-            player->ADD_GOSSIP_ITEM(ICON_GOSSIP_BALOON, "Продать", GOSSIP_SENDER_MAIN, ACTION_SELL_GUILDHOUSE_OPT);
+            if (action == ACTION_SELL_GUILDHOUSE)
+            {
+                int i = -1;
+                try
+                {
+                    //compare code
+
+                    if (strlen(sCode) + 1 == sizeof CODE_SELL)
+                        i = strcmp(CODE_SELL, sCode);
+                }
+
+                catch(char *str) {sLog->outErrorDb(str);
+                }
+
+                if (i == 0)
+                {
+                    //right code
+                    sellGuildhouse(player, _creature);
+                }
+
+                player->CLOSE_GOSSIP_MENU();
+                return true;
+            }
         }
-        else
-        {
-            //show additional menu for guild leader
-            player->ADD_GOSSIP_ITEM(ICON_GOSSIP_GOLD, MSG_GOSSIP_BUY, GOSSIP_SENDER_MAIN, ACTION_SHOW_BUYLIST);
-        }
+        return false;
     }
 
-    player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, _creature->GetGUID());
-    return true;
- }
+    bool OnGossipSelect(Player *player, Creature *_creature, uint32 sender, uint32 action)
+    {
+        player->PlayerTalkClass->ClearMenus();
+        if (sender != GOSSIP_SENDER_MAIN)
+            return false;
+
+        switch (action)
+        {
+        case ACTION_TELE:
+            //teleport player to GH
+            player->CLOSE_GOSSIP_MENU();
+            teleportPlayerToGuildHouse(player, _creature);
+            break;
+        case ACTION_SHOW_BUYLIST:
+            //show list of GHs which currently not occupied
+            showBuyList(player, _creature);
+            break;
+
+        case ACTION_SELL_GUILDHOUSE_OPT:
+            player->CLOSE_GOSSIP_MENU();
+            player->ADD_GOSSIP_ITEM(0, "Да, Продать Гильдхаус", GOSSIP_SENDER_MAIN, ACTION_SELL_GUILDHOUSE_YES);
+            player->ADD_GOSSIP_ITEM(0, "Нет, закрыть окно", GOSSIP_SENDER_MAIN, ACTION_SELL_GUILDHOUSE_NO);
+            player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, _creature->GetGUID());
+            break;
+
+        case ACTION_SELL_GUILDHOUSE_YES:
+            sellGuildhouse(player, _creature);
+            player->CLOSE_GOSSIP_MENU();
+            break;
+
+        case ACTION_SELL_GUILDHOUSE_NO:
+            player->CLOSE_GOSSIP_MENU();
+            break;
+
+        default:
+            if (action > OFFSET_SHOWBUY_FROM)
+            {
+                showBuyList(player, _creature, action - OFFSET_SHOWBUY_FROM);
+            }
+            else if (action > OFFSET_GH_ID_TO_ACTION)
+            {
+                //player clicked on buy list
+                player->CLOSE_GOSSIP_MENU();
+
+                //get guildhouseId from action
+                //guildhouseId = action - OFFSET_GH_ID_TO_ACTION
+                buyGuildhouse(player, _creature, action - OFFSET_GH_ID_TO_ACTION);
+            }
+            break;
+        }
+
+        return true;
+    }
+
+
+    bool OnGossipHello(Player *player, Creature *_creature)
+    {
+        player->ADD_GOSSIP_ITEM(ICON_GOSSIP_BALOON, MSG_GOSSIP_TELE, GOSSIP_SENDER_MAIN, ACTION_TELE);
+
+        if (isPlayerGuildLeader(player))
+        {
+            if (isPlayerHasGuildhouse(player, _creature))
+            {
+                player->ADD_GOSSIP_ITEM(ICON_GOSSIP_BALOON, "Продать", GOSSIP_SENDER_MAIN, ACTION_SELL_GUILDHOUSE_OPT);
+            }
+            else
+            {
+                //show additional menu for guild leader
+                player->ADD_GOSSIP_ITEM(ICON_GOSSIP_GOLD, MSG_GOSSIP_BUY, GOSSIP_SENDER_MAIN, ACTION_SHOW_BUYLIST);
+            }
+        }
+
+        player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, _creature->GetGUID());
+        return true;
+    }
 
 
 };
 void AddSC_guildmaster()
 {
- new guildmaster();
+    new guildmaster();
 }

zeroR2/mangos

fix possible crush in Player::CheckAreaExploreAndOutdoor

src/game/Player.cpp
@@ -6414,7 +6414,7 @@ void Player::CheckAreaExploreAndOutdoor()
     if (!isAlive())
         return;
 
-    if (IsTaxiFlying())
+    if (IsTaxiFlying() || !GetMap())
         return;
 
     bool isOutdoor;

artkeep/trinity

Signed-off-by: Chaplain <aionthefirst@gmail.com>

src/server/game/Entities/Player/Player.cpp
@@ -6809,7 +6809,7 @@ void Player::CheckAreaExploreAndOutdoor()
     if (!isAlive())
         return;
 
-    if (isInFlight())
+    if (isInFlight() || !GetMap())
         return;
 
     bool isOutdoor;